5401481c00816ec780976d94b66815032440ffce
Yelyzaveta Vecherovska <vecheli0306@gmail.com>
2026-01-30 17:11:28 +0200

    update files pixabay-api.js, render-functions.js, index.html, base.css

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,14 +1,61 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const lightbox = new SimpleLightbox('.gallery', {
+const lightbox = new SimpleLightbox('.gallery a', {
   captions: true,
   captionsData: 'alt',
   captionPosition: 'bottom',
   captionDelay: 250,
 });
 
-function createGallery(images) {}
-function clearGallery() {}
-function showLoader() {}
-function hideLoader() {}
+const galleryItems = document.querySelector('.gallery');
+const loaderItem = document.querySelector('.loader');
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+      <a class="gallery-link" href="${largeImageURL}">
+      <div class="photo-item">
+        <img
+          class="gallery-image"
+          src="${webformatURL}"
+          alt="${tags}"
+        />
+        <div class="photo-information">
+        <p class="information-item">Likes ${likes}</p>
+                <p class="information-item">Views ${views}</p>
+        <p class="information-item">Comments ${comments}</p>
+        <p class="information-item">Downloads ${downloads}</p>
+</div>
+</div>
+      </a>
+      </li>
+  `
+    )
+    .join('');
+
+  galleryItems.insertAdjacentHTML('beforeend', markup);
+
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryItems.innerHTML = '';
+}
+
+export function showLoader() {
+  loaderItem.classList.remove('is-hidden');
+}
+export function hideLoader() {
+  loaderItem.classList.add('is-hidden');
+}
